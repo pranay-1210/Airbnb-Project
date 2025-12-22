8be5f28d29cb71a3b04056e3347e7bc4b03e4022
Pranay <praveenpranay07@gmail.com>
2025-12-22 15:04:29 +0530

    Created app.js file and created a middleware for 404 Page not Found with the help of Express

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded());
+
+
+app.use((req, res, next) => {
+  console.log("First Middleware", req.url, req.method, req.body);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.statusCode = 404;
+  res.write(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+            <title>Page Not Found</title>
+            </head>
+            <body>
+            <h1>404 Page Not Found</h1>
+            </body>
+            </html>
+        `);
+  res.end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
+
