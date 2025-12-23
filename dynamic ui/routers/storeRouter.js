a6809dd693cddce22ac65c87eb8a257238cad115
Pranay <praveenpranay07@gmail.com>
2025-12-23 11:25:19 +0530

    used the ejs syntax to paint the ui by writing the javascript code inside the html file

--- dynamic ui/routers/storeRouter.js
+++ dynamic ui/routers/storeRouter.js
@@ -8,7 +8,8 @@ const { registeredHomes } = require("./hostRouter");
 
 storeRouter.get("/", (req, res,next) => {
   console.log(registeredHomes);
-  res.sendFile(path.join(rootDir, "views", "index.html"));
+  // res.sendFile(path.join(rootDir, "views", "index.html"));
+  res.render('index', { homes: registeredHomes }); //for rendering the registered homes using ejs template engine
  });
 
 module.exports = storeRouter;
