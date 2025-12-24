e57b886d0ea0e649ad5d992aa202d12f3eb75db9
Pranay <praveenpranay07@gmail.com>
2025-12-24 12:03:40 +0530

    Created Favourites model with static methods, connected it to controllers, fetched home details, and redesigned favourites page content.

--- Dynamic Path/controllers/storeController.js
+++ Dynamic Path/controllers/storeController.js
@@ -1,39 +1,54 @@
 const Home = require("./../models/Home");
+const Favourite = require("../models/Favourite");
 
-exports.getIndex = (req, res,next) => {
-  Home.fetchAll(registeredHomes => {
-    res.render('store/index', { homes: registeredHomes, pagetTitle: "Our Airbnb" });
-  }); 
+exports.getIndex = (req, res, next) => {
+  Home.fetchAll((registeredHomes) => {
+    res.render("store/index", {
+      homes: registeredHomes,
+      pagetTitle: "Our Airbnb",
+    });
+  });
 };
 
-
-exports.getHomes = (req, res,next) => {
-  Home.fetchAll(registeredHomes => {
-    res.render('store/homes', { homes: registeredHomes, pagetTitle: "Our Airbnb" });
-  }); 
+exports.getHomes = (req, res, next) => {
+  Home.fetchAll((registeredHomes) => {
+    res.render("store/homes", {
+      homes: registeredHomes,
+      pagetTitle: "Our Airbnb",
+    });
+  });
 };
 
-exports.getFavourites = (req, res,next) => {
-  Home.fetchAll(registeredHomes => {
-    res.render('store/favourites', { homes: registeredHomes, pagetTitle: "Favourites" });
-  }); 
+exports.getFavourites = (req, res, next) => {
+  Favourite.fetchAll((favouriteIds) => {
+    Home.fetchAll((registeredHomes) => {
+      const favouriteHomes = registeredHomes.filter(home => favouriteIds.includes(home.id));
+      res.render("store/favourites", {
+        homes: favouriteHomes,
+        pagetTitle: "Favourites",
+      });
+    });
+  })
+}
+
+exports.postAddFavourites = (req, res, next) => {
+  // console.log("Came to add favourites",req.body);
+  const homeId = req.body.id;
+  Favourite.addToFavourites(homeId, (error) => {
+    if (error) {
+      console.log("Error while adding to favourites", error);
+    }
+    res.redirect("/favourites");
+  });
 };
 
-exports.postAddFavourites = (req, res,next) => {
-  console.log("Came to add favourites",req.body);
-  res.redirect('/favourites');
-};
-
-
-
-exports.getHomeDetails = (req, res,next) => {
+exports.getHomeDetails = (req, res, next) => {
   const homeId = req.params.homeIdentity;
-  Home.findById(homeId, home => {
-    if(!home) {
+  Home.findById(homeId, (home) => {
+    if (!home) {
       console.log("Home not found");
       return res.redirect("/homes");
     }
-    res.render('store/home-detail', { home: home, pagetTitle: "Home Detail" });
+    res.render("store/home-detail", { home: home, pagetTitle: "Home Detail" });
   });
-}
-  
+};
